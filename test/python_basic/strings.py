e7b4684a61e10aaf607162323df0173d7e56450b
Nazym <nazymduisen08@gmail.com>
2026-01-31 10:46:16 +0500

    added strings file

---
+++ test/python_basic/strings.py
@@ -0,0 +1,21 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = "Hello, World!"
+print(a.lower())
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
