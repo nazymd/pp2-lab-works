5f14d112fca0eb6c3f3852204f39c749965ca94b
Nazym <nazymduisen08@gmail.com>
2026-02-04 18:12:29 +0500

    Add If Else examples

---
+++ practice2/if_else/if_else.py
@@ -0,0 +1,36 @@
+a = 200
+b = 33
+if b > a: #33>200 False
+  print("b is greater than a")
+else:#Do
+  print("b is not greater than a")
+
+number = 7
+if number % 2 == 0:#7%2=1 True
+  print("The number is even")
+else:
+  print("The number is odd")
+
+username = "Emil"
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
+temperature = 22
+if temperature > 30:
+  print("It's hot outside!")
+else:
+  print("It's cold outside!")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+    if a == b:
+        print("a and b are equal")
+    else:
+        print("a is greater than b")
