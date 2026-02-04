e6423c77e9de7ad4306b6aa267a6148f839197e8
Nazym <nazymduisen08@gmail.com>
2026-02-04 18:27:10 +0500

    Add If Elif Else examples

---
+++ practice2/if_else/if_elif_else.py
@@ -0,0 +1,58 @@
+age = 25
+if age < 13:
+  print("You are a child")
+elif age < 20:
+  print("You are a teenager")
+elif age < 65:
+  print("You are an adult")
+else:
+  print("You are a senior")
+
+
+day = 3
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+else:
+  print("Sunday")
+
+
+score = 75
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+else:
+  print("Grade: D")
+
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+temperature = 22
+if temperature > 30:
+  print("It's hot outside!")
+elif temperature > 20:
+  print("It's warm outside")
+elif temperature > 10:
+  print("It's cool outside")
+else:
+  print("It's cold outside!")
