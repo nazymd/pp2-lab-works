f10fcaaf58c0b7b41e837cad6797861dc3932ab1
Nazym <nazymduisen08@gmail.com>
2026-02-04 15:09:52 +0500

    Add If Statement examples

---
+++ practice2/if_else/if_statement.py
@@ -0,0 +1,25 @@
+a = 33
+b = 200
+if b > a: #200>33 True
+  print("b is greater than a")
+
+number = 15
+if number > 0: #15 > 0 True
+  print("The number is positive")
+
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+
+number = -10
+if number < 0:
+  print("The number is negative")
