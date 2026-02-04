87b6459d3d46f571084c321235c0512e30b0b0ae
Nazym <nazymduisen08@gmail.com>
2026-02-04 14:24:32 +0500

    Add Booleans as Comparison Results examples

---
+++ practice2/boolean/boolean_comparison.py
@@ -0,0 +1,23 @@
+print(10 > 9)  #True
+print(10 == 9) #False
+print(10 < 9)  #False
+
+a = 200
+b = 33
+if b > a:  #33>200  #False
+  print("b is greater than a")
+else: 
+  print("b is not greater than a")
+
+print("cat" == "cat")     #True
+print("cat" == "Cat")     #False
+print("hello" != "world") #True
+
+print("Apple" > "apple")  #False
+print("Zebra" < "apple")  #True
+
+print("cat" < "caterpillar")  #True len("cat") < len("caterpillar")
+print("dat" < "caterpillar")  #False d > c
+print("book" > "boo")         #True
+print("10" < "2")             #True 1 < 2
+
