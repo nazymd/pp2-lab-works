edcf19b659731d54932e280b275898f4ba0979b9
Nazym <nazymduisen08@gmail.com>
2026-02-06 15:05:19 +0500

    Add While Loops examples

---
+++ practice2/loops/while_loop.py
@@ -0,0 +1,48 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+n = 5
+while n > 0:
+    print(n)
+    n -= 1
+print("Hello!")
+# 5 
+# 4 
+# 3
+# 2 
+# 1
+# Hello!
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+i = 1
+sum = 0
+while i <= 100:
+    sum += i#1+2+3.....100
+    i += 1
+print("Summa:", sum)#5050
+
+i = 1
+n=int(input())
+fact=1
+while i <= n:
+    fact*=i
+    i+=1
+print(fact)
