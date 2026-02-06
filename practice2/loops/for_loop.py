468d7342b0841bbb17a09d3a62ef7d0285b8b687
Nazym <nazymduisen08@gmail.com>
2026-02-06 15:42:17 +0500

    Add For Loops  examples

---
+++ practice2/loops/for_loop.py
@@ -0,0 +1,24 @@
+#Print each fruit in a fruit list:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+  
+# Loop through the letters in the word "banana":
+for x in "banana":
+  print(x)
+
+
+for x in range(6):
+  print(x)
+
+
+for x in range(2, 6):
+  print(x)
+
+
+for x in range(2, 30, 3):
+  print(x)
+
+
+
