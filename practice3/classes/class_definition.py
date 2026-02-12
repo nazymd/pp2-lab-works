1f60dc0638e7fb2f4b638df8832f37388f70dfcf
Nazym <nazymduisen08@gmail.com>
2026-02-12 16:30:21 +0500

    Added class.definition examples

---
+++ practice3/classes/class_definition.py
@@ -0,0 +1,37 @@
+class MyClass:
+  x = 5
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+
+class Person:
+  pass
+
+
+class family:
+   father="Duisen"
+   mother="Aigul"
+   son="Balgynbek"
+print(family.son)
+
+
+class Car:
+    wheels = 4
+c1 = Car()
+print(c1.wheels)
+
+
+class Student:
+   name="Nazym"
+   age = 17
+print(Student.name)
+print(Student.age)
+
+
+
+
+
