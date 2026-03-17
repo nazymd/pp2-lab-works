bedc06824fa42df32955159b4ae3a447e2f40bfc
Nazym <nazymduisen08@gmail.com>
2026-03-17 11:19:26 +0500

    Added write file codes

---
+++ practice6/file_handling/write_files.py
@@ -0,0 +1,3 @@
+with open("data.txt", "a") as f:
+  f.write("Now the file has more content!")
+  f.write("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.")
