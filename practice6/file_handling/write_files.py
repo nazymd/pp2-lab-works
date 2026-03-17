b48350f21b3d802e7752c789d9f205d11d33feb7
Nazym <nazymduisen08@gmail.com>
2026-03-17 11:30:22 +0500

    Added write file codes

--- practice6/file_handling/write_files.py
+++ practice6/file_handling/write_files.py
@@ -1,3 +1,10 @@
 with open("data.txt", "a") as f:
   f.write("Now the file has more content!")
   f.write("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.")
+with open ("data.txt", 'r') as f:
+  print(f.read())
+
+with open("data.txt", "w") as f:
+    f.write("Hello")
+with open ("data.txt", 'r') as f:
+  print(f.read())
